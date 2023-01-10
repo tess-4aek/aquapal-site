77508e7e5b34a7261cc24d6950560eeb6b996663
tess-4aek <ruslan.vyshnevskyi@nitrix.io>
2023-01-10 11:05:49 +0300

    fix_100123_4

--- src/sections/Memories.jsx
+++ src/sections/Memories.jsx
@@ -26,6 +26,7 @@ const Memories = () => {
     useEffect(() => {
         const memoriesOffsetTop = ref.current
         dispatch(setMemoriesPosAction(memoriesOffsetTop.offsetTop))
+        document.querySelector('.swiper-wrapper').style.transform = 'translateX(-2335px) !important'
     }, [])
 
 
